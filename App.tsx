6ad4847e680fee54945c5c4f2a03d2f1ed178b9e
Gaurav Bisht <gaurav.bisht@tothenew.com>
2024-10-12 12:47:37 +0530

    dynamic radio buttons

--- App.tsx
+++ App.tsx
@@ -13,11 +13,47 @@ import MyRadioButtons from './customComponents/myRadioButtons';
 const App = () => {
 
   const [resetSelection, setResetSelection] = useState(false);
+  const [selectedRadio, setSelectedRadio] = useState(0);
+
+  const radioButtonArray = [
+    {
+      title: 'Radio 1'
+    },
+    {
+      title: 'Radio 2'
+    },
+    {
+      title: 'Radio 3'
+    },
+    {
+      title: 'Radio 4'
+    },
+    {
+      title: 'Radio 5'
+    },
+    {
+      title: 'Radio 6'
+    },
+    {
+      title: 'Radio 7'
+    }
+  ];
 
   return (
     <SafeAreaView style={styles.mySafeAreaView}>
       <View style={styles.myMainContainerView}>
-        <MyRadioButtons resetSelection={resetSelection} setResetSelection={setResetSelection} />
+        {
+          radioButtonArray.map((item, index) => {
+            return (
+              <MyRadioButtons index={index}
+                title={item.title}
+                resetSelection={resetSelection}
+                setResetSelection={setResetSelection}
+                selectedRadio={selectedRadio}
+                setSelectedRadio={setSelectedRadio} />
+            )
+          })
+        }
         <View style={{ flex: 1, margin: 10 }}>
           <Button title='Reset selection' onPress={() => setResetSelection(true)} />
         </View>
--- customComponents/myRadioButtons.js
+++ customComponents/myRadioButtons.js
@@ -2,26 +2,20 @@ import { View, Text, TouchableOpacity, Button } from 'react-native';
 import { useEffect, useState } from 'react';
 
 const MyRadioButtons = (props) => {
-    const [selectedRadio, setSelectedRadio] = useState(0);
+    const title = props.title
+    const index = props.index
 
     useEffect(() => {
-        props.resetSelection && setSelectedRadio(0);
+        props.resetSelection && props.setSelectedRadio(0);
         props.setResetSelection(false);
     });
 
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
-            <TouchableOpacity style={{ marginStart: 20, marginEnd: 20 }} onPress={() => setSelectedRadio(1)}>
+            <TouchableOpacity style={{ marginStart: 20, marginEnd: 20 }} onPress={() => props.setSelectedRadio(index + 1)}>
                 <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <View style={{ borderColor: 'black', borderWidth: 1, width: 10, height: 10, borderRadius: 5, backgroundColor: selectedRadio === 1 ? 'black' : 'transparent' }}></View>
-                    <Text style={{ paddingStart: 10 }}>Radio 1</Text>
-                </View>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={{ marginStart: 20, marginEnd: 20 }} onPress={() => setSelectedRadio(2)}>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <View style={{ borderColor: 'black', borderWidth: 1, width: 10, height: 10, borderRadius: 5, backgroundColor: selectedRadio === 2 ? 'black' : 'transparent' }}></View>
-                    <Text style={{ paddingStart: 10 }}>Radio 2</Text>
+                    <View style={{ borderColor: 'black', borderWidth: 1, width: 10, height: 10, borderRadius: 5, backgroundColor: props.selectedRadio === (index + 1) ? 'black' : 'transparent' }}></View>
+                    <Text style={{ paddingStart: 10 }}>{title}</Text>
                 </View>
             </TouchableOpacity>
         </View>
